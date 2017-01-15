75aa13bfef9e292534b92fecf7627a22c4f99f37
leico <yoshitaka_12@iamas.ac.jp>
2017-01-15 20:50:47 +0900

    add vimr initialize script and gitignore file
    vimr initialize script
    	install dein.vim
    
    gitignore file
    	ignore dein.vim caches

---
+++ vimrinit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+deindir="$HOME/.config/nvim/dein";
+reposdir="$HOME/.config/nvim/dein/repos";
+
+if [ -e $reposdir ]; then
+  rm -rf $reposdir
+fi
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh $deindir
+rm ./installer.sh
+
